0bda881a97832b49c21ae8feafa2d800e2329f94
sauravtonde <2021bcs080@sggs.ac.in>
2025-01-28 09:20:01 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import connectDB from './db/db.js';
 
 dotenv.config({ path: './.env' });  // Load the environment variables
 
-connectDB();  // Connect to MongoDB
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!", err);
+  })
+
+
+
+
+// Connect to MongoDB

e1753a0ecfb81f03ad3c6d05879a3f570f1c63ef
sauravtonde <2021bcs080@sggs.ac.in>
2025-01-29 11:35:40 +0530

    models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
+import express from 'express'
+
 import connectDB from './db/db.js';
 
 dotenv.config({ path: './.env' });  // Load the environment variables
+
+const app = express()
+
 
 connectDB()
   .then(() => {

85bdc8470ec69ebd9f08a76822e59f6bdacb20de
sauravtonde <2021bcs080@sggs.ac.in>
2025-02-04 12:26:05 +0530

    all setup configured

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express'
 
-import connectDB from './db/db.js';
+import connectDB from './src/db/db.js';
 
 dotenv.config({ path: './.env' });  // Load the environment variables
 
